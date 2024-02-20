2a2a32fcd517609e7c054c97e8f5d27b92e22bdc
Dmitry Brezgunov <brezgunov.dmitri@gmail.com>
2024-02-20 17:10:50 +0300

    add server busy error

--- pubchempy2/errors.py
+++ pubchempy2/errors.py
@@ -14,6 +14,8 @@ def handle_http_error(response: requests.Response):
             raise MethodNotAllowedError(msg)
         case 504:
             raise TimeoutError(msg)
+        case 503:
+            raise SeverBusyError(msg)
         case 501:
             raise UnimplementedError(msg)
         case 500:
@@ -32,28 +34,28 @@ class PubChemHTTPError(PubChemPyError):
     """Generic error class to handle all HTTP error codes."""
 
     def __init__(self, msg="Unspecified HTTP error"):
-        self.msg = msg
+        super().__init__(msg)
 
 
 class BadRequestError(PubChemHTTPError):
     """Request is improperly formed (syntax error in the URL, POST body, etc.)."""
 
     def __init__(self, msg="Request is improperly formed"):
-        self.msg = msg
+        super().__init__(msg)
 
 
 class NotFoundError(PubChemHTTPError):
     """The input record was not found (e.g. invalid CID)."""
 
     def __init__(self, msg="The input record was not found"):
-        self.msg = msg
+        super().__init__(msg)
 
 
 class MethodNotAllowedError(PubChemHTTPError):
     """Request not allowed (such as invalid MIME type in the HTTP Accept header)."""
 
     def __init__(self, msg="Request not allowed"):
-        self.msg = msg
+        super().__init__(msg)
 
 
 class TimeoutError(PubChemHTTPError):
@@ -63,18 +65,25 @@ class TimeoutError(PubChemHTTPError):
     """
 
     def __init__(self, msg="The request timed out"):
-        self.msg = msg
+        super().__init__(msg)
 
 
 class UnimplementedError(PubChemHTTPError):
     """The requested operation has not (yet) been implemented by the server."""
 
     def __init__(self, msg="The requested operation has not been implemented"):
-        self.msg = msg
+        super().__init__(msg)
 
 
 class ServerError(PubChemHTTPError):
     """Some problem on the server side (such as a database server down, etc.)."""
 
     def __init__(self, msg="Some problem on the server side"):
-        self.msg = msg
+        super().__init__(msg)
+
+
+class SeverBusyError(PubChemHTTPError):
+    """Too many requests or server is busy, retry later"""
+
+    def __init__(self, msg="Too many requests or server is busy, retry later"):
+        super().__init__(msg)
